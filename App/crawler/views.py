dc93f3be66b8d98709050fb2f0e0bf19a424d21c
heju <heju@sobey.com>
2020-03-18 10:00:30 +0800

    爬取测试代码

--- App/crawler/views.py
+++ App/crawler/views.py
@@ -1,11 +1,15 @@
 from . import crawler
 from flask import request, jsonify
+import requests
+from bs4 import BeautifulSoup as bs4
 
 
 @crawler.route('/test', methods=['POST'])
-def add_character():
+def test():
     # requestData = request.get_json(request.form)
-    print(request.form['aaaa'])
+    ret = request.form.to_dict()
+    print(ret['aaaa'])
+    print(request.form)
     responseData = [
             {
                 "id": 1,
@@ -16,4 +20,20 @@ def add_character():
                 "newNote": ""
             }
         ]
-    return jsonify(request.form)
+    ret['test'] = '12313'
+    return jsonify(ret)
+
+
+@crawler.route('/testCrawler', methods=['POST'])
+def testCrawler():
+    r = requests.get('https://www.720mp4.com/')
+    r.encoding = 'gb2312'
+    soup = bs4(r.text, 'lxml')
+    title_all = soup.find_all(class_="title_all")
+    today_item = ''
+    for item in title_all:
+        if (item.find('span').text == '今日更新'):
+            today_item = item
+    print(today_item.next_sibling)
+    res = '123'
+    return res
