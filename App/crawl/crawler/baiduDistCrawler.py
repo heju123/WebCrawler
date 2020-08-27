6807c939350b120045f6eda5d8b58e9ae1b66bc6
heju <heju@sobey.com>
2020-08-27 20:17:08 +0800

    按顺序爬取

--- App/crawl/crawler/baiduDistCrawler.py
+++ App/crawl/crawler/baiduDistCrawler.py
@@ -21,6 +21,18 @@ class BaiduDistCrawler:
         if (len(list_all) > 0):
             for item in list_all:
                 h3 = item.find(class_='x-left-h3')
-                print(h3.find('a').attrs['href'])
-                print(h3.find('a').text)
+                detail_href = h3.find('a').attrs['href']
+                if detail_href:
+                    try:
+                        detail_r = requests.get(detail_href)
+                        detail_r.encoding = 'utf-8'
+                        detail_soup = bs4(detail_r.text, 'lxml')
+                        key_btns = detail_soup.find_all(class_='main-xzfx-a')
+                        if (key_btns and len(key_btns) > 0):
+                            for key_btn in key_btns:
+                                if (key_btn.text == '进入百度网盘下载'):
+                                    print(h3.find('a').text)
+                                    print(key_btn.attrs['href'])
+                    except Exception as e:
+                        print(e)
             self.crawlSowangpan(page + 1)
